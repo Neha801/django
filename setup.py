8fabdd30dbdc0d49bbaab2e232e6fdae647d435e
Neha Kumari <55044120+Neha801@users.noreply.github.com>
2020-05-08 15:09:38 +0530

    installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+pip intallvirtualenvwrapper_bin
+mkvirtualenv test  ---#name of environment is test
+pip intall django
+django--admin--version -- #show version of you django installed
+mkdir projects      --#name of folder
+cd project           --# move into your project
+django-admin start project jangos   -- #it will start the project and name th eproject file as jangos
+cd jangos              --#move to project file
+python manange.py runserver  - #run the server
+
+now visit http://localhost:8000/
